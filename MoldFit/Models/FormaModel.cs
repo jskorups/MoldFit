8a689cffd5f048542086056070c8d15fc0ec274a
Jakub Skorupski <skorupski.jakub@gmail.com>
2019-08-20 23:58:41 +0200

    20082019 #1

---
+++ MoldFit/Models/FormaModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoldFit.Models
+{
+    public class FormaModel
+    {
+        public FormaModel(int id)
+        {
+            MoldFitEntities ent = new MoldFitEntities();
+            var fm = ent.Forma.Where(x => x.id == id).FirstOrDefault();
+            fm.CopyProperties(this);
+        }
+
+        public FormaModel(string A, string B, string C, int ProjektId)
+        {
+            this.WymiarA = A;
+            this.WymiarB = B;
+            
+        }
+
+        public int id { get; set; }
+        public string WymiarA { get; set; }
+        public string WymiarB { get; set; }
+        public string WymiarC { get; set; }
+        public Nullable<int> ProjektId { get; set; }
+
+        public ProjektModel Projekt { get { return GetProject(); } }
+
+        protected ProjektModel GetProject()
+        {
+            if (!this.ProjektId.HasValue) return null;
+            return new ProjektModel(this.ProjektId.Value);
+        }
+
+    }
+}
---
+++ MoldFit/Models/ProjektModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoldFit.Models
+{
+    public class ProjektModel
+    {
+        public ProjektModel(int id)
+        {
+            MoldFitEntities ent = new MoldFitEntities();
+            var pr = ent.ProjektTest.Where(x => x.id == id).FirstOrDefault();
+            pr.CopyProperties(this);
+        }
+
+        public ProjektModel(ProjektTest p)
+        {
+            p.CopyProperties(this);
+        }
+
+        public int id { get; set; }
+        public string ProjektNazwa { get; set; }
+
+        public List<FormaModel> Formy { get; set; }
+
+        public static List<ProjektModel> GetProjectList()
+        {
+            MoldFitEntities ent = new MoldFitEntities();
+            return ent.ProjektTest.ToList().Select(x => new ProjektModel(x)).ToList();
+        }
+    }
+}
